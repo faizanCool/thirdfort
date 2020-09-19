dfc287dfa8210515c880c26579c48e1a84a089f2
MF Jaleel <mohamedfaizan56@gmail.com>
2020-09-19 15:01:25 +0530

    util class was added

---
+++ src/main/java/com/thirdfort/note/util/Util.java
@@ -0,0 +1,10 @@
+package com.thirdfort.note.util;
+
+public class Util {
+    public static final Integer MAX_NOTE_LENGTH = 250;
+
+    public static class ERROR_MESSAGE {
+        public static final String INVALID_NOTE = "Invalid note entered";
+        public static final String EXCEED_NOTE_SIZE = "Exceeds maximum number of characters";
+    }
+}
